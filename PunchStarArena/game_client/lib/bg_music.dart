1f436d0a28dd3863946c94fb131ea4fcc07780ee
Sabrina-rgb9 <selmeftah3.cf@iesesteveterradas.cat>
2026-05-11 17:53:01 +0200

    musica borrada

--- PunchStarArena/game_client/lib/bg_music.dart
+++ PunchStarArena/game_client/lib/bg_music.dart
@@ -5,39 +5,13 @@ class BgMusic {
   static final BgMusic _instance = BgMusic._();
   static BgMusic get instance => _instance;
 
-  final AudioPlayer _menuPlayer = AudioPlayer();
-  final AudioPlayer _gameplayPlayer = AudioPlayer();
-  bool _menuPlaying = false;
-  bool _gameplayPlaying = false;
+  Future<void> play() async {}
 
-  Future<void> play() async {
-    if (_menuPlaying) return;
-    _menuPlaying = true;
-    await _menuPlayer.setReleaseMode(ReleaseMode.loop);
-    await _menuPlayer.play(AssetSource('audio/japanesechiptune.mp3'));
-  }
+  Future<void> stop() async {}
 
-  Future<void> stop() async {
-    if (!_menuPlaying) return;
-    _menuPlaying = false;
-    await _menuPlayer.stop();
-  }
+  Future<void> playGameplay() async {}
 
-  Future<void> playGameplay() async {
-    if (_gameplayPlaying) return;
-    _gameplayPlaying = true;
-    await _gameplayPlayer.setReleaseMode(ReleaseMode.loop);
-    await _gameplayPlayer.play(AssetSource('audio/EternityLivesLoop.wav'));
-  }
+  Future<void> stopGameplay() async {}
 
-  Future<void> stopGameplay() async {
-    if (!_gameplayPlaying) return;
-    _gameplayPlaying = false;
-    await _gameplayPlayer.stop();
-  }
-
-  Future<void> disposePlayer() async {
-    await _menuPlayer.dispose();
-    await _gameplayPlayer.dispose();
-  }
+  Future<void> disposePlayer() async {}
 }
